3005b847cf4f9aecdd0f9b894812674478e38c07
jdwoolcock <30227772+jdwoolcock@users.noreply.github.com>
2019-02-21 15:46:20 +0000

    Initial revision

---
+++ hostbuilder/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace HostTest
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            // Configuration providers
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            // createSerilog logger
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(config)
+                .CreateLogger();
+
+            var hostBuilder = new HostBuilder()
+                .ConfigureServices((hostContext, services) =>
+                {
+
+                }).UseSerilog();
+
+            Log.Information("Starting host");
+
+            await hostBuilder.RunConsoleAsync();
+        }
+    }
+}
