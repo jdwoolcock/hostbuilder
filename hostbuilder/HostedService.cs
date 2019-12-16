4278de6bd333954fcf8bdebf38d9897059204924
jdwoolcock <30227772+jdwoolcock@users.noreply.github.com>
2019-12-16 09:15:36 +0000

    Use .net core 3.1

---
+++ hostbuilder/HostedService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace hostbuilder
+{
+    public class HostedService : BackgroundService
+    {
+        private readonly ILogger logger;
+
+        public HostedService(ILogger<HostedService> logger)
+        {
+            this.logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            logger.LogInformation("Service starting.");
+
+            stoppingToken.Register(() => logger.LogInformation("Service has been  stopped."));
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                logger.LogInformation("Service is doing background work.");
+
+                await Task.Delay(TimeSpan.FromSeconds(5), stoppingToken);
+            }
+
+            logger.LogInformation("Service has finished.");
+        }
+    }
+}
--- hostbuilder/Program.cs
+++ hostbuilder/Program.cs
@@ -1,34 +1,34 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
-
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Serilog;
+using Microsoft.Extensions.Logging;
 
-namespace HostTest
+namespace hostbuilder
 {
-    class Program
+    public class Program
     {
         public static async Task Main(string[] args)
         {
-            // Configuration providers
-            var config = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json")
-                .Build();
-
-            // createSerilog logger
-            Log.Logger = new LoggerConfiguration()
-                .ReadFrom.Configuration(config)
-                .CreateLogger();
-
-            var hostBuilder = new HostBuilder()
-                .ConfigureServices((hostContext, services) =>
-                {
-
-                }).UseSerilog();
-
-            Log.Information("Starting host");
-
-            await hostBuilder.RunConsoleAsync();
+            try
+            {
+                await new HostBuilder()
+                    .ConfigureAppConfiguration((hostContext, configBuilder) =>
+                    {
+                        configBuilder.AddJsonFile("appsettings.json");
+                    })
+                    .ConfigureLogging((_, config) =>
+                    {
+                        config.AddConsole();
+                    })
+                    .ConfigureServices(services => services.AddHostedService<HostedService>())
+                    .RunConsoleAsync();
+            }
+            catch (Exception e)
+            {
+                Console.Write(e.Message);
+            }
         }
     }
 }
